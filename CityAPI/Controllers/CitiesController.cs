3c0aca570a57e8baf415fdc1abd1a3c5aaa5cedf
if164ever1 <acere5ihor@gmail.com>
2023-08-18 23:47:11 +0200

    add first controller

---
+++ CityAPI/Controllers/CitiesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet("jsonresult")]
+        public IActionResult JsonResult()
+        {
+            var cities = new[]
+            {
+                new {id = 1, Name = "Lwiw"},
+                new {id = 2, Name = "Ivano-Frankivsk"}
+            };
+
+            return Ok(cities);
+        }
+    }
+}
