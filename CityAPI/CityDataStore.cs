ad658f572dae64f714a3aa0a9b68c53e7d5c50e2
if164ever1 <acere5ihor@gmail.com>
2023-08-20 08:43:24 +0200

    add model city

---
+++ CityAPI/CityDataStore.cs
@@ -0,0 +1,32 @@
+﻿using CityAPI.Model;
+
+namespace CityAPI
+{
+    public class CityDataStore
+    {
+        public  List<CityDto> cities { get; set; }
+
+        public static CityDataStore Instance { get; } = new CityDataStore();
+        public CityDataStore() 
+        {
+            cities = new List<CityDto>()
+            {
+                new CityDto()
+                { 
+                    Id = 1,
+                    Name = "Lwiw",
+                },
+                new CityDto()
+                {
+                    Id= 2,
+                    Name = "Ivano-Frankivsk"
+                },
+                new CityDto() 
+                {
+                    Id = 3,
+                    Name = "Ternopil"
+                }
+            };
+        }
+    }
+}
---
+++ CityAPI/Model/CityDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CityAPI.Model
+{
+    public class CityDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
